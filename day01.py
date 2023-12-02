2df08d205919c2a64703211cd115b60ef1ed3aa6
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-02 16:07:10 +0100

    day01

---
+++ day01.py
@@ -0,0 +1,31 @@
+import re, sys
+from baseday import BaseDay
+
+class Day01(BaseDay):
+
+    def part1(self):
+        total = 0
+        with open(self.input) as f:
+            for line in f:
+                digits = [c for c in line if c.isdigit()]
+                total += int(digits[0] + digits[-1])
+        print("day1 part1: ", total)
+
+    def part2(self):
+        digitmap = {
+            'zero': 0 ,'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+            'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
+            '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+        }
+        digits = '|'.join(digitmap.keys())
+        total = 0
+        with open(self.input) as f:
+            for line in f:
+                first = digitmap[re.search(f"^.*?({digits}).*", line).group(1)]
+                last = digitmap[re.search(f"^.*({digits})", line).group(1)]
+                total += first * 10 + last
+        print("day1 part2: ", total)
+
+if __name__ == "__main__":
+    day = Day01(sys.argv[1])
+    day.run()
--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
 
-days = [ ]
+import day01
+
+days = [ day01.Day01 ]
 
 for day in days:
     instance = day()
