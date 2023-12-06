3daaed9ed99fbe6fc818d44f56462307c76cc884
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-06 15:11:11 +0100

    day06 part 1+2

--- day06-wait-for-it/day06.py
+++ day06-wait-for-it/day06.py
@@ -1,25 +1,34 @@
 from baseday import BaseDay
 from dataclasses import dataclass
-from typing import Iterable
+from functools import reduce
 
 @dataclass
 class Race:
     time:   int
     distance: int
 
+    def wins(self) -> int:
+        count = 0
+        for t in range(1, self.time):
+            dist = (self.time - t) * t;
+            count += dist > self.distance
+        return count
+
 class Day06(BaseDay):
-    races: list[Race]
+    races1: list[Race]
+    race2: Race
 
     def init(self) -> None:
-        self.races = []
+        self.races1 = []
         with open(self.input) as f:
             lines = [x.split()[1:] for x in f]
             for n in range(0, len(lines[0])):
-                self.races.append(Race(lines[0][n], lines[1][n]))
-        print('races:', self.races)
+                self.races1.append(Race(int(lines[0][n]), int(lines[1][n])))
+            self.race2 = Race(int("".join(lines[0])), int("".join(lines[1])))
 
     def part1(self) -> None:
-        print('day06 part1:', 'TBD')
+        score = reduce(lambda x, y: x*y, [race.wins() for race in self.races1])
+        print('day06 part1:', score)
 
     def part2(self) -> None:
-        print('day06 part2:', 'TBD')
+        print('day06 part2:', self.race2.wins())
