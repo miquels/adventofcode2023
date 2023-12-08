cb9c5d9bab66b71595a2df4600753749418b2edc
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-08 12:46:02 +0100

    day08 part 2

--- day08-haunted-wasteland/day08.py
+++ day08-haunted-wasteland/day08.py
@@ -1,25 +1,20 @@
-import re
+import math, re
 from baseday import BaseDay
 from dataclasses import dataclass
-from typing import Self
+from typing import Callable, Self
 
 @dataclass
 class Node:
     name: str
     left: str
     right: str
-    final: bool
 
     @classmethod
     def parse(cls, line: str) -> Self:
-        name, left, right = re.search(r'(\w+) = \((\w+), (\w+)\)', line).groups()
-        return cls(name, left, right, name.endswith('Z'))
-
-    def step(self, step: str, nodes: dict[str, 'Node']) -> 'Node':
-        if step == 'L':
-            return nodes[self.left]
-        else:
-            return nodes[self.right]
+        matches = re.search(r'(\w+) = \((\w+), (\w+)\)', line)
+        if matches is None:
+            raise ValueError('failed to parse input line')
+        return cls(*matches.groups())
 
 class Day08(BaseDay):
     steps: str
@@ -34,32 +29,20 @@ class Day08(BaseDay):
                 node = Node.parse(line)
                 self.nodes[node.name] = node
 
-    def part1(self) -> None:
-        node = self.nodes['AAA']
-        done = False
+    def count(self, node: Node, stop: Callable[[Node], bool]) -> int:
         steps = 0
-        while not done:
+        while True:
             for step in [*self.steps]:
                 steps += 1
-                node = node.step(step, self.nodes)
-                if node.name == 'ZZZ':
-                    done = True
-                    break
+                node = self.nodes[node.left] if step == 'L' else self.nodes[node.right]
+                if stop(node):
+                    return steps
+
+    def part1(self) -> None:
+        steps = self.count(self.nodes['AAA'], lambda x: x.name == 'ZZZ')
         print('day08 part1:', steps)
 
     def part2(self) -> None:
         nodes = [node for name, node in self.nodes.items() if name.endswith('A')]
-        numnodes = len(nodes)
-        done = False
-        steps = 0
-        while not done:
-            for step in [*self.steps]:
-                steps += 1
-                final = 0
-                for i in range(0, numnodes):
-                    nodes[i] = nodes[i].step(step, self.nodes)
-                    final += nodes[i].final
-                if final == numnodes:
-                    done = True
-                    break
-        print('day08 part2:', steps)
+        cycles = [self.count(node, lambda x: x.name.endswith('Z')) for node in nodes]
+        print('day08 part2:', math.lcm(*cycles))
