7f0e3cbcf47cb7547c0fe23483289a6d6d7a9e8f
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-09 15:10:34 +0100

    day09 part 2

--- day09-mirage-maintenance/day09.py
+++ day09-mirage-maintenance/day09.py
@@ -1,5 +1,13 @@
 from baseday import BaseDay
+from functools import reduce
 from itertools import pairwise
+
+def calc(numbers: list[int], index: int) -> list[int]:
+    nums = [ numbers ]
+    while True:
+        nums.append([n[1] - n[0] for n in pairwise(nums[-1])])
+        if sum(nums[-1]) == 0:
+            return [num[index] for num in nums]
 
 class Day09(BaseDay):
     lines: list[list[int]]
@@ -9,14 +17,9 @@ class Day09(BaseDay):
             self.lines = [list(map(int, line.split())) for line in f]
 
     def part1(self) -> None:
-        total = 0
-        for nums in self.lines:
-            while True:
-                total += nums[-1]
-                nums = [n[1] - n[0] for n in pairwise(nums)]
-                if sum(nums) == 0:
-                    break
+        total = sum(sum(calc(line, -1)) for line in self.lines)
         print("day09 part1:", total)
 
     def part2(self) -> None:
-        print("day09 part2:", 'TBD')
+        total = sum(reduce(lambda x, y: y - x, calc(line, 0)[::-1]) for line in self.lines)
+        print("day09 part2:", total)
