e8bdcd88ea0215e95240368e1191e21d5f2a26f7
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-10 14:37:47 +0100

    day10: simplify

--- day10-pipe-maze/day10.py
+++ day10-pipe-maze/day10.py
@@ -1,112 +1,70 @@
 from baseday import BaseDay
 from dataclasses import dataclass
-from itertools import repeat
-from typing import Self
+from itertools import count, repeat
+from typing import Any, ClassVar, Self
 
 @dataclass
 class Position:
     y: int
     x: int
+    dirs: ClassVar[Any] = { 'N': (-1, 0), 'S': (1, 0), 'E': (0, -1), 'W': (0, 1) }
 
-    def north(self) -> 'Position':
-        return Position(self.y - 1, self.x)
-
-    def south(self) -> 'Position':
-        return Position(self.y + 1, self.x)
-
-    def east(self) -> 'Position':
-        return Position(self.y, self.x - 1)
-
-    def west(self) -> 'Position':
-        return Position(self.y, self.x + 1)
+    def to(self, dir: str) -> 'Position':
+        d = self.dirs[dir]
+        return Position(self.y + d[0], self.x + d[1])
 
 @dataclass
-class Pipe:
+class Element:
     type: str
+    dirs: ClassVar[Any] = { 'N': '|LJ', 'S': '|7F', 'E': '-J7', 'W': '-LF' }
 
-    def can_north(self) -> bool:
-        return self.type in '|LJ'
-
-    def can_south(self) -> bool:
-        return self.type in '|7F'
-
-    def can_east(self) -> bool:
-        return self.type in '-J7'
-
-    def can_west(self) -> bool:
-        return self.type in '-LF'
+    def can(self, dir: str) -> bool:
+        return self.type in self.dirs[dir]
 
 @dataclass
 class Maze:
-    grid: list[list[Pipe]]
+    grid: list[list[Element]]
     start: Position
     pos: Position
     prevpos: Position
 
-    def at(self, pos: Position) -> Pipe:
+    def at(self, pos: Position) -> Element:
         return self.grid[pos.y][pos.x]
 
-    def north(self) -> Pipe:
-        return self.at(self.pos.north())
-
-    def south(self) -> Pipe:
-        return self.at(self.pos.south())
-
-    def east(self) -> Pipe:
-        return self.at(self.pos.east())
-
-    def west(self) -> Pipe:
-        return self.at(self.pos.west())
+    def to(self, dir: str) -> Element:
+        return self.at(self.pos.to(dir))
 
     def step(self) -> None:
-        pipe = self.at(self.pos)
+        elem = self.at(self.pos)
         nxt = self.pos
-        if pipe.can_north() and self.pos.north() != self.prevpos:
-            nxt = self.pos.north()
-        if pipe.can_south() and self.pos.south() != self.prevpos:
-            nxt = self.pos.south()
-        if pipe.can_east() and self.pos.east() != self.prevpos:
-            nxt = self.pos.east()
-        if pipe.can_west() and self.pos.west() != self.prevpos:
-            nxt = self.pos.west()
+        for dir in ['N', 'S', 'E', 'W']:
+            if elem.can(dir) and (nxt := self.pos.to(dir)) != self.prevpos:
+                break
         self.prevpos = self.pos
         self.pos = nxt
 
     @classmethod
     def parse(cls, input: str) -> Self:
         grid = []
+        start: Position
         with open(input) as f:
-            for line in f:
-                grid.append([Pipe(c) for c in line.strip()])
-                grid[-1].append(Pipe('.'))
-        grid.append(list(repeat(Pipe('.'), len(grid[0]) + 1)))
-
-        max_y = len(grid) - 1
-        max_x = len(grid[0]) - 1
-        start = Position(0, 0)
-        for y in range(0, max_y):
-            for x in range(0, max_x):
-                if grid[y][x].type == 'S':
+            for y, line in enumerate(f):
+                grid.append([Element(c) for c in line.strip()])
+                grid[-1].append(Element('.'))
+                if (x := line.find('S')) >= 0:
                     start = Position(y, x)
-                    break
-        maze = cls(grid, start, start, Position(-1, -1))
+        grid.append(list(repeat(Element('.'), len(grid[0]) + 1)))
+        return cls(grid, start, start, Position(-1, -1))
 
-        p = '.'
-        if maze.north().can_south() and maze.south().can_north():
-            p = '|'
-        if maze.north().can_south() and maze.east().can_west():
-            p = 'J'
-        if maze.north().can_south() and maze.west().can_east():
-            p = 'L'
-        if maze.south().can_north() and maze.east().can_west():
-            p = '7'
-        if maze.south().can_north() and maze.west().can_east():
-            p = 'F'
-        if maze.east().can_west() and maze.west().can_east():
-            p = '-'
-        maze.at(start).type = p
-
-        return maze
+    def run(self) -> int:
+        for d1, d2 in [('N', 'S'), ('S', 'N'), ('E', 'W'), ('W', 'E')]:
+            if (e := self.to(d1)) and e.can(d2):
+                self.pos = self.pos.to(d1)
+        for steps in count(2):
+            self.step()
+            if self.pos == self.start:
+                break
+        return steps
 
 class Day10(BaseDay):
     maze: Maze
@@ -115,13 +73,8 @@ class Day10(BaseDay):
         self.maze = Maze.parse(self.input)
 
     def part1(self) -> None:
-        steps = 0
-        while True:
-            self.maze.step()
-            steps += 1
-            if self.maze.pos == self.maze.start:
-                break
-        print("day10 part1:", int(steps / 2))
+        val = self.maze.run()
+        print("day10 part1:", val // 2)
 
     def part2(self) -> None:
         print("day10 part2:", 'TBD')
