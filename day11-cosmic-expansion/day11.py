f662211160652e3aae06e2fc22528d11760f2ce2
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-12 00:38:58 +0100

    day11 part 2

--- day11-cosmic-expansion/day11.py
+++ day11-cosmic-expansion/day11.py
@@ -1,7 +1,7 @@
 from baseday import BaseDay
 from dataclasses import dataclass
 from itertools import combinations
-from typing import Any, ClassVar, Iterator, Self
+from typing import Self
 
 def transpose(array: list[list[bool]]) -> list[list[bool]]:
     a = list(zip(*array))
@@ -12,24 +12,30 @@ def expansions(grid: list[list[bool]]) -> list[int]:
 
 @dataclass
 class Universe:
-    stars: list[tuple[int, int]]
+    galaxies: list[tuple[int, int]]
+    exp_y: list[int]
+    exp_x: list[int]
 
     @classmethod
     def parse(cls, input: str) -> Self:
         grid: list[list[bool]] = []
-        stars = []
+        galaxies = []
         with open(input) as f:
             for y, line in enumerate([line.strip() for line in f]):
                 grid.append([c == '#' for c in [*line]])
-                stars += [(y, x) for x in range(0, len(line)) if line[x] == '#']
+                galaxies += [(y, x) for x in range(0, len(line)) if line[x] == '#']
         exp_y = expansions(grid)
         exp_x = expansions(transpose(grid))
-        for i in range(0, len(stars)):
-            star = stars[i]
-            dy = sum([star[0] > y for y in exp_y])
-            dx = sum([star[1] > x for x in exp_x])
-            stars[i] = (star[0] + dy, star[1] + dx)
-        return cls(stars)
+        return cls(galaxies, exp_y, exp_x)
+
+    def calc(self, factor: int) -> int:
+        galaxies = []
+        for galaxy in self.galaxies:
+            dy = sum([galaxy[0] > y for y in self.exp_y])
+            dx = sum([galaxy[1] > x for x in self.exp_x])
+            galaxies.append((galaxy[0] + factor * dy, galaxy[1] + factor * dx))
+        sets = combinations(galaxies, 2)
+        return sum(abs(g0[0] - g1[0]) + abs(g0[1] - g1[1]) for g0, g1 in sets)
 
 class Day11(BaseDay):
     universe: Universe
@@ -38,11 +44,7 @@ class Day11(BaseDay):
         self.universe = Universe.parse(self.input)
 
     def part1(self) -> None:
-        sets = combinations(self.universe.stars, 2)
-        total = 0
-        for s0, s1 in sets:
-            total += abs(s0[0] - s1[0]) + abs(s0[1] - s1[1])
-        print("day11 part1:", total)
+        print("day11 part1:", self.universe.calc(1))
 
     def part2(self) -> None:
-        print("day11 part2:", 'TBD')
+        print("day11 part2:", self.universe.calc(999999))
