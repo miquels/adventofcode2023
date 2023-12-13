5b67575dbc9b19fea3ee62a0614e8203a3d418f2
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-13 13:07:00 +0100

    cleanup

--- day13-point-of-incidence/day13.py
+++ day13-point-of-incidence/day13.py
@@ -7,16 +7,15 @@ class Pattern:
     grid: list[str]
 
     def reflection(self, smudges: int) -> int:
-        for y in range(0, len(self.grid) - 1):
+        for y in range(len(self.grid) - 1):
             if self.reflects(y) == smudges:
                 return y + 1
         return 0
 
     def reflects(self, y: int) -> int:
-        diff = 0
-        y1, y2 = y, y + 1
+        diff, y1, y2 = 0, y, y + 1
         while y1 >= 0 and y2 < len(self.grid):
-            diff += sum([self.grid[y1][x] != self.grid[y2][x] for x in range(0, len(self.grid[0]))])
+            diff += sum([self.grid[y1][x] != self.grid[y2][x] for x in range(len(self.grid[0]))])
             if diff > 1:
                 break
             y1 -= 1
@@ -24,15 +23,10 @@ class Pattern:
         return diff
 
     def transpose(self) -> 'Pattern':
-        grid = []
-        for x in range(0, len(self.grid[0])):
-            grid.append("".join([self.grid[y][x] for y in range(len(self.grid) -1, -1, -1)]))
-        return Pattern(grid)
+        return Pattern(["".join(x) for x in [*zip(*self.grid)]])
 
     def score(self, smudges: int) -> int:
-        total = self.transpose().reflection(smudges)
-        total += 100 * self.reflection(smudges)
-        return total
+        return self.transpose().reflection(smudges) + 100 * self.reflection(smudges)
 
 class Day13(BaseDay):
     patterns: list[Pattern]
@@ -40,8 +34,7 @@ class Day13(BaseDay):
     def init(self) -> None:
         with open(self.input) as file:
             data = file.read()
-        grids = data.strip("\n").split("\n\n")
-        self.patterns = [Pattern(grid.split("\n")) for grid in grids]
+        self.patterns = [Pattern(grid.splitlines()) for grid in data.split("\n\n")]
 
     def part1(self) -> None:
         print("day13 part1:", sum([p.score(0) for p in self.patterns]))
