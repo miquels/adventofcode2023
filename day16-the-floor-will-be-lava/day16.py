f027223ed129835fc37fddfe77c6297e56fa8b40
Miquel van Smoorenburg <mike@langeraar.net>
2023-12-17 18:27:16 +0100

    day16 part 2

--- day16-the-floor-will-be-lava/day16.py
+++ day16-the-floor-will-be-lava/day16.py
@@ -19,7 +19,17 @@ class Contraption:
             grid.append([Elem(e, set()) for e in [*line]])
         return cls(grid, len(grid[0]))
 
-    def run(self, y: int, x: int, dy: int, dx: int) -> None:
+    def run(self, y: int, x: int, dy: int, dx: int) -> int:
+        for elems in self.grid:
+            for elem in elems:
+                elem.dirs.clear()
+        self.subrun(y, x, dy, dx)
+        total = 0
+        for elems in self.grid:
+            total += sum([len(elem.dirs) != 0 for elem in elems])
+        return total
+
+    def subrun(self, y: int, x: int, dy: int, dx: int) -> None:
         while y >= 0 and y < self.size and x >= 0 and x < self.size:
             e = self.grid[y][x]
             if (dy, dx) in e.dirs:
@@ -33,22 +43,16 @@ class Contraption:
                     dx, dy = dy, dx
                 case '-':
                     if dx == 0:
-                        self.run(y, x + 1, 0, 1)
-                        self.run(y, x - 1, 0, -1)
+                        self.subrun(y, x + 1, 0, 1)
+                        self.subrun(y, x - 1, 0, -1)
                         return
                 case '|':
                     if dy == 0:
-                        self.run(y + 1, x, 1, 0)
-                        self.run(y - 1, x, -1, 0)
+                        self.subrun(y + 1, x, 1, 0)
+                        self.subrun(y - 1, x, -1, 0)
                         return
             y += dy
             x += dx
-
-    def score(self) -> int:
-        total = 0
-        for elems in self.grid:
-            total += sum([len(elem.dirs) != 0 for elem in elems])
-        return total
 
 class Day16(BaseDay):
     c: Contraption
@@ -59,7 +63,14 @@ class Day16(BaseDay):
 
     def part1(self) -> None:
         self.c.run(0, 0, 0, 1)
-        print("day16 part1:", self.c.score())
+        print("day16 part1:", self.c.run(0, 0, 0, 1))
 
     def part2(self) -> None:
-        print("day16 part2:", 'TBD')
+        scores = []
+        for y in range(self.c.size):
+            scores.append(self.c.run(y, 0, 0, 1))
+            scores.append(self.c.run(y, self.c.size -1, 0, -1))
+        for x in range(self.c.size):
+            scores.append(self.c.run(0, x, 1, 0))
+            scores.append(self.c.run(self.c.size -1, x, -1, 0))
+        print("day16 part2:", max(scores))
